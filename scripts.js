bd2b3bbfc71d0914c13884704f6de4d811b616ee
Cas Wilkinson <46457060+hailcassie@users.noreply.github.com>
2019-02-04 14:57:48 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,32 @@
 // Interacting
+
+var trigger = document.querySelector('.trigger')
+var body = document.querySelector('body')
+
+trigger.addEventListener('click', zoom)
+
+function zoom() {
+	console.log('it works')
+
+	if (body.id != 'zoom') {
+		body.id = 'zoom'
+	} else {
+		body.id = ''
+	}
+
+}
+
+
+// Partial eclipse on hover
+var earth = document.querySelector('.earth')
+
+earth.addEventListener('mouseenter', showShadow)
+earth.addEventListener('mouseleave', hideShadow)
+
+function showShadow() {
+	earth.classList.add('eclipse')
+}
+
+function hideShadow() {
+	earth.classList.remove('eclipse')
+}
